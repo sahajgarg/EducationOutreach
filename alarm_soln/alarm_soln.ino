70890e44fc2b45602a18195d6c9bc91b9abb1c82
Sahaj Garg <sahajgarg@gmail.com>
2017-02-23 22:02:54 -0800

    fixed soln

--- alarm_soln/alarm_soln.ino
+++ alarm_soln/alarm_soln.ino
@@ -24,7 +24,7 @@ const int ALARM = 3;
 float sinVal;
 int toneVal;
 bool triggered;
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, 300);
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, 1000);
 
 void setup() { 
   // setup code runs once when the program starts
